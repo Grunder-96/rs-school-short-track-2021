3f72df392c0bf8d7832c4c6f047e9df909578205
Grunder-96 <grunder-96@mail.ru>
2021-04-08 11:39:01 +0300

    feat: task 7 complete

--- src/07-file-names.js
+++ src/07-file-names.js
@@ -13,8 +13,29 @@
  * the output should be ["file", "file(1)", "image", "file(1)(1)", "file(2)"]
  *
  */
-function renameFiles(/* names */) {
-  throw new Error('Not implemented');
+function renameFiles(names) {
+  const answer = names.slice();
+  function searchDuble(value, array, index) {
+    let pos = 0;
+    const v = value;
+    const a = array;
+    let i = index;
+    for (i; i < answer.length; i++) {
+      if (v === a[i]) {
+        if (pos === 0) {
+          a[i] = `${a[i]}`;
+        } else {
+          a[i] = `${a[i]}(${pos})`;
+        }
+        pos++;
+      }
+    }
+    pos = 0;
+  }
+  for (let i = 0; i < answer.length; i++) {
+    searchDuble(answer[i], answer, i);
+  }
+  return answer;
 }
 
 module.exports = renameFiles;
