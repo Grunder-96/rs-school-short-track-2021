20087bbe4dbee78aa0b24c4b938512e60c393aa7
Grunder-96 <grunder-96@mail.ru>
2021-04-09 10:37:28 +0300

    feat: complete task 11

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,25 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  let answer = '';
+  let check = 1;
+  let arg = str[0];
+  for (let i = 0; i < str.length; i++) {
+    if (str[i + 1] === str[i]) {
+      check++;
+      arg = str[i];
+    } else {
+      if (check === 1) {
+        answer += arg;
+      } else {
+        answer += `${check}${arg}`;
+      }
+      arg = str[i + 1];
+      check = 1;
+    }
+  }
+  return answer;
 }
 
 module.exports = encodeLine;
