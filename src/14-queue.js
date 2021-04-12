d282db3402a7feb29088ee50a8110b07f10bacb7
Grunder-96 <grunder-96@mail.ru>
2021-04-12 12:27:26 +0300

    feat: complete task 14

--- src/14-queue.js
+++ src/14-queue.js
@@ -1,27 +1,45 @@
-// const ListNode = require('../extensions/list-node');
-/**
- * Implement the Queue with a given interface via linked list (use ListNode extension above).
- *
- * @example
- * const queue = new Queue();
- *
- * queue.enqueue(1); // adds the element to the queue
- * queue.enqueue(3); // adds the element to the queue
- * queue.dequeue(); // returns the top element from queue and deletes it, returns 1
- *
- */
+const ListNode = require('../extensions/list-node');
+//  * Implement the Queue with a given interface via linked list (use ListNode extension above).
+//  *
+//  * @example
+//  * const queue = new Queue();
+//  *
+//  * queue.enqueue(1); // adds the element to the queue
+//  * queue.enqueue(3); // adds the element to the queue
+//  * queue.dequeue(); // returns the top element from queue and deletes it, returns 1
+//  *
+//  */
 
 class Queue {
-  get size() {
-    throw new Error('Not implemented');
+  constructor() {
+    this.length = 0;
+    this.head = null;
+    this.tail = null;
   }
 
-  enqueue(/* element */) {
-    throw new Error('Not implemented');
+  get size() {
+    return this.length;
+  }
+
+  enqueue(element) {
+    const node = new ListNode(element);
+    node.value = element;
+    // есть ли хотя бы один элемент в очереди
+    if (this.head) {
+      this.tail.next = node;
+      this.tail = node;
+    } else {
+      this.head = node;
+      this.tail = node;
+    }
+    this.length++;
   }
 
   dequeue() {
-    throw new Error('Not implemented');
+    const current = this.head;
+    this.head = this.head.next;
+    this.length--;
+    return current.value;
   }
 }
 
